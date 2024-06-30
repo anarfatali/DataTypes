a3557b70f3882be0c90707c3c14b4ad5e16c1668
anarouis <133520254+Fatali-ishere@users.noreply.github.com>
2024-06-30 18:59:32 +0400

    first commit

---
+++ src/House.java
@@ -0,0 +1,12 @@
+public class House {
+    int numberOfBedrooms = 3;
+    int numberOfBathrooms = 2;
+    boolean hasPool = false;
+    boolean hasGarden = true;
+    double price = 259999.99f;
+    int yearBuilt = 2004;
+    boolean hasGarage = true;
+    int squareFeet = 2000;
+    int numberOfFloors = 2;
+    String address = "Khirdalan";
+}
